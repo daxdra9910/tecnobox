71fdb96485618c5dd066ef2dbffea1a8a77067f5
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2023-05-30 17:26:41 -0500

    Add: Get products on home.

--- apps/shop/views/Home.py
+++ apps/shop/views/Home.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
 from django.views import View
 
-
+from apps.shop.models import Product
 
 
 class Home(View):
 
     def get(self, request):
+
+        products = Product.objects.all()
+
         context = {
             'user': request.user,
-            'path': request.path
+            'path': request.path,
+            'products' : products
         }
         return render(request, 'shop/home.html', context)
