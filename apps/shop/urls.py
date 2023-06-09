98c5fa22e869f6138e5af9ddfb416b9f9ebfaf47
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2023-06-09 12:30:05 -0500

    Add: Urls for Products and Discounts views.

--- apps/shop/urls.py
+++ apps/shop/urls.py
@@ -12,8 +12,10 @@ urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('about-us/', views.AboutUs.as_view(), name='about-us'),
     path('contact-us/', views.ContactUs.as_view(), name='contact-us'),
+    path('products/', views.Products.as_view(), name='products'),
     path('products/<int:product_id>/', views.Detail.as_view(), name='detail'),
     path('products/<int:product_id>/make-review/', views.MakeReview.as_view(), name='make-review'),
+    path('discounts/',views.Discounts.as_view(), name='discounts' ),
     path('cart/<int:cart_id>/', views.Cart.as_view(), name='cart'),
     path('terms-and-conditions/', views.TermsAndConditions.as_view(), name='terms-and-conditions'),
 ]
--- apps/shop/views/__init__.py
+++ apps/shop/views/__init__.py
@@ -4,3 +4,5 @@ from apps.shop.views.Detail import *
 from apps.shop.views.Home import *
 from apps.shop.views.Cart import *
 from apps.shop.views.TermsAndConditions import *
+from apps.shop.views.Products import *
+from apps.shop.views.Discounts import *
