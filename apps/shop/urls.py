179c9dfd97bebea43815c0ee533ea5fb42ee7672
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2023-06-18 23:19:08 -0500

    Add: Path of cart methods.

--- apps/shop/urls.py
+++ apps/shop/urls.py
@@ -16,6 +16,11 @@ urlpatterns = [
     path('products/<int:product_id>/', views.Detail.as_view(), name='detail'),
     path('products/<int:product_id>/make-review/', views.MakeReview.as_view(), name='make-review'),
     path('discounts/',views.Discounts.as_view(), name='discounts' ),
-    path('cart/<int:cart_id>/', views.Cart.as_view(), name='cart'),
+    path('cart/', views.Cart.as_view(), name='cart'),
+    path('increase-quantity/<int:product_id>/', views.Cart.increase_quantity, name='increase-quantity'),
+    path('decrease-quantity/<int:product_id>/', views.Cart.decrease_quantity, name='decrease-quantity'),
+    path('remove-from-cart/<int:product_id>/', views.Cart.remove_from_cart, name='remove-from-cart'),
+    path('empty-car/', views.Cart.empty_cart, name='empty-cart'),
+    path('purchase', views.Cart.purchase, name='purchase'),
     path('terms-and-conditions/', views.TermsAndConditions.as_view(), name='terms-and-conditions'),
 ]
